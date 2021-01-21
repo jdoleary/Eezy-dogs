1d0953056c8840dad4ab1e3008d5269b6c2ad766
Jordan O'Leary <jdoleary@gmail.com>
2021-01-21 08:05:32 -0500

    clean: App.tsx; Fetch main data; use PairChooser

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,48 @@
-import React, { Suspense } from 'react';
-import { Button, TextField } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/lab';
-import { Provider, useSelector} from 'react-redux'
-import {store, incremented} from './store'
+import React, {useEffect} from 'react';
+import { Button, Container } from '@material-ui/core';
+
+import { Provider, useSelector } from 'react-redux';
+import { store, addPair, Pair, State } from './store';
+import {fetchAllBreeds} from './breed/actions'
+import BreedPairChooser from './breed/PairChooser'
 import './App.css';
 
-const SelectCounter = (state:any) => state.value
+const SelectPairs = (state: State): Pair[] => state.pairs;
 
-import { fetchProfileData } from "./fakeApi";
-
-const resource = fetchProfileData();
 interface AppProps {}
 
 function App({}: AppProps) {
+  useEffect(() => {
+    store.dispatch(fetchAllBreeds());
+  }, [])
   return (
     <div className="App">
       <Provider store={store}>
-      <Suspense
-        fallback={<h1>Loading posts...</h1>}
-      >
-        <ProfileTimeline />
-      </Suspense>
-        <Button color="primary" onClick={()=>{store.dispatch(incremented())}}>Material UI</Button>
+        <Choosers />
+        <Button
+          color="primary"
+          onClick={() => {
+            store.dispatch(
+              addPair({ breed: 'one', subBreed: 'two', count: 4 }),
+            );
+          }}
+        >
+          Generate
+        </Button>
       </Provider>
     </div>
   );
 }
-function ProfileTimeline() {
-  const counter = useSelector(SelectCounter)
-  // Try to read posts, although they might not have loaded yet
-  const posts = resource.posts.read();
+function Choosers() {
+  const pairs = useSelector(SelectPairs);
   return (
-    <ul>
-      <pre>{JSON.stringify(counter, null, 2)}</pre>
-      {Object.keys(posts).slice(0,4).map((post:any) => (
-        <li key={post}>{post}</li>
-      ))}
-      <Autocomplete
-  id="breed"
-  options={Object.keys(posts)}
-  getOptionLabel={(option:any) => option}
-  style={{ width: 300 }}
-  renderInput={(params) => <TextField {...params} label="Breed" variant="outlined" />}
-/>
-    </ul>
+    <Container maxWidth="md">
+      {pairs.map((p, i) => {
+        return (
+          <BreedPairChooser key={`${p.breed}-${p.subBreed}`} breed={p.breed} includeAddBtn={i === pairs.length-1}/>
+        );
+      })}
+    </Container>
   );
 }
 
