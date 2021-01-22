7a3af502ccc9a1f28247f2a9db34e2fa9d7fa64c
Jordan O'Leary <jdoleary@gmail.com>
2021-01-21 21:34:33 -0500

    src: Open modal when images change and exist

--- src/imageGrid/Modal.tsx
+++ src/imageGrid/Modal.tsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
-import { Dialog, Box } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Dialog, GridList, GridListTile } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-import { SelectPairs } from '../store';
+import { SelectImages } from '../store';
 
 export default function Modal() {
-
-  const [isModalOpen, setIsModalOpen] = useState(true);
-  const pairs = useSelector(SelectPairs);
-  return (<Dialog
-          open={isModalOpen}
-          onClose={() => {
-            setIsModalOpen(false);
-          }}
-        >
-          <Box p={4}>{JSON.stringify(pairs)}</Box>
-        </Dialog>
-  )
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const images = useSelector(SelectImages);
+  useEffect(() => {
+    if(images.length !== 0){
+      setIsModalOpen(true)
+    }
+  }, [images])
+  return (
+    <Dialog
+      open={isModalOpen}
+      onClose={() => {
+        setIsModalOpen(false);
+      }}
+    >
+      <GridList cellHeight={160} cols={3}>
+        {images.map((img) => (
+          <GridListTile key={img} cols={1}>
+            <img src={img} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </Dialog>
+  );
 }
