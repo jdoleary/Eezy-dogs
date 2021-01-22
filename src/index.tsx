e1a129a8187f5676fc22681babcfff2a0e17c16c
Jordan O'Leary <jdoleary@gmail.com>
2021-01-21 19:55:03 -0500

    style: Add custom global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: green[600],
+    },
+  },
+  spacing: 8,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
