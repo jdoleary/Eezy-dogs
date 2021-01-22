d96a5865b10d7a4e8d1348025f59318d407f2803
Jordan O'Leary <jdoleary@gmail.com>
2021-01-21 19:39:52 -0500

    src: Make Selector a Controlled Component

--- src/breed/Selector.tsx
+++ src/breed/Selector.tsx
@@ -3,10 +3,14 @@ import { TextField } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 interface SelectorProps {
   name: string;
+  value: string;
+  onChange: any;
   options: string[];
 }
 export default function Selector({
   name,
+  value,
+  onChange,
   options = [],
 }: SelectorProps) {
   return (
@@ -15,6 +19,8 @@ export default function Selector({
       disabled={options.length === 0}
       options={options}
       getOptionLabel={(option: string) => option}
+      value={value}
+      onChange={onChange}
       style={{ width: 300 }}
       renderInput={(params) => (
         <TextField {...params} label={name} variant="outlined" />
